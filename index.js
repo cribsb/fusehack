7cb63fe69694ef72304fb9656210863f1e8ab9f7
Chris Bogaards <chrisbogaards.cb@gmail.com>
2015-11-07 21:28:45 +0100

    log

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var Game = IgeClass.extend({
 	classId: 'Game',
 
 	init: function (App, options) {
-		Console.log("1");
+		console.log("1");
 		// Create the engine
 		ige = new IgeEngine();
-		Console.log("2");
+		console.log("2");
 		if (ige.isClient) {
 			ige.client = new App();
 		}
-		Console.log("3");
+		console.log("3");
 		if (ige.isServer) {
 			ige.server = new App(options);
 		}
